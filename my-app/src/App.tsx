d86caf745ef70759de6793a1867f5edf608cd566
Tatiana <tanik.sam@yandex.ru>
2022-10-03 09:48:09 +0300

    fix: fix router path

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -48,7 +48,7 @@ function App() {
         </header>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/nofound" element={<NotFoundPage />} />
+          <Route path="*" element={<NotFoundPage />} />
           <Route path="/about" element={<AboutUs />} />
         </Routes>
         <footer className="footer">
